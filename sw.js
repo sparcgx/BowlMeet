8b0103d84f4f93efdf059a024df1d17099b39cec
sparcgx <sparcgx@gmail.com>
2026-09-22 07:21:26 +0800

    Refresh PWA cache for v0.4.4-R1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='bowlmeet-v0.4.4-dev.4-history-search-filter-shell';
-const RUNTIME='bowlmeet-v0.4.4-dev.4-history-search-filter-runtime';
+const CACHE='bowlmeet-v0.4.4-R1-regression-data-safety-shell';
+const RUNTIME='bowlmeet-v0.4.4-R1-regression-data-safety-runtime';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k!==RUNTIME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
