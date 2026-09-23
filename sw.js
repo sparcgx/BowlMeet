0e071760bbbb680af81699c97356dc08a18b9fb1
sparcgx <sparcgx@gmail.com>
2026-09-24 07:41:12 +0800

    release: v0.4.6-RC.1 Liquid Glass regression gate

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='bowlmeet-v0.4.5.2-version-display-shell';
-const RUNTIME='bowlmeet-v0.4.5.2-version-display-runtime';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
+const CACHE='bowlmeet-v0.4.6-rc1-shell';
+const RUNTIME='bowlmeet-v0.4.6-rc1-runtime';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./assets/liquid-glass-dev4.css','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k!==RUNTIME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting()});
