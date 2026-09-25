a175623048efd05279474b013ff84c3af468ee04
sparcgx <sparcgx@gmail.com>
2026-09-25 10:12:35 +0800

    v0.4.6.2.1-dev.1: rotate PWA cache for history grid fix

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const PRODUCTION_CACHE_ROOT='bowlmeet-v';
-const CACHE='bowlmeet-v0.4.6.2-stable-shell';
-const RUNTIME='bowlmeet-v0.4.6.2-stable-runtime';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest','./assets/liquid-glass-dev4.css','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
+const CACHE='bowlmeet-v0.4.6.2.1-dev.1-gridfix-shell';
+const RUNTIME='bowlmeet-v0.4.6.2.1-dev.1-gridfix-runtime';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./assets/liquid-glass-dev4.css?v=04621d1','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)))});
 
