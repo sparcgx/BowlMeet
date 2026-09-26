6909a85557d4c17d1af4f3ce3bee2c5c92f17bca
sparcgx <sparcgx@gmail.com>
2026-09-26 09:13:19 +0800

    v0.4.7.0-dev.3-R1: refresh PWA cache for mobile nav alignment fix

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const PRODUCTION_CACHE_ROOT='bowlmeet-v';
-const CACHE='bowlmeet-v0.4.7.0-dev.3-mobile-shell';
-const RUNTIME='bowlmeet-v0.4.7.0-dev.3-mobile-runtime';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest','./assets/liquid-glass-dev4.css?v=0470d3','./assets/theme-center-dev2.css?v=0470d3','./assets/theme-center-dev3-mobile.css?v=0470d3','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
+const CACHE='bowlmeet-v0.4.7.0-dev.3-r1-mobile-shell';
+const RUNTIME='bowlmeet-v0.4.7.0-dev.3-r1-mobile-runtime';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./assets/liquid-glass-dev4.css?v=0470d3','./assets/theme-center-dev2.css?v=0470d3','./assets/theme-center-dev3-mobile.css?v=0470d3r1','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)))});
 
